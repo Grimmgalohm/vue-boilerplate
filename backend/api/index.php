11a053e28c97df4cf33e651b7cb884b6a8375c65
cesar_luna <cesarlunalimon@gmail.com>
2022-07-13 20:49:28 -0500

    config commit

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,1 +1,17 @@
-$null 
+<?php
+switch($_SERVER['REQUEST_METHOD']){
+    case "POST":
+        echo "post";
+        break;
+    case "PUT":
+        echo "put";
+        break;
+    case 'DELETE':
+        echo "delete";
+        break;
+    case 'GET':
+        echo "break";
+        break;
+}
+
+?>
